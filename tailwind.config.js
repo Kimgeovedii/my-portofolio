65c3999496a263d8a69d6e8db618f5eba1a2a087
itlppan <it.lppan.id@gmail.com>
2024-07-06 21:20:27 +0700

    portofolio tailwiind css

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      container:{
+          center:true,
+          padding:'16px',
+      },
+    extend: {
+      animation: {
+          'fade-in': 'fadeIn 1s ease-out forwards',
+        },
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: 0 },
+            '100%': { opacity: 1 },
+          },
+        },
+      colors: {
+          'nav' : '#1e293b',
+          'dark': '#0f172a',
+          'primary' : '#8b5cf6',
+          'secondary' : '#a78bfa',
+          'tulisan': '#94a3b8',
+          'paragraf' : '#6b7280',
+      },
+      screens:{
+          '2xl' :'1320px',
+      }
+    },
+  },
+  plugins: [],
+}
